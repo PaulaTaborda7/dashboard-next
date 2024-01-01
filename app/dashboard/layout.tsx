9f321920de8ae6f57168ebf13eb5c428c889855a
PaulaTaborda7 <ptabordam@unal.edu.co>
2023-12-31 23:22:45 -0500

    Layout of the dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,15 @@
+import SideNav from "../ui/dashboard/sidenav"
+
 export default function Layout (
     { children } : { children: React.ReactNode } // Export a default functions, and the we put the type of the return, because this is typescript
-)
+    )
 {
     return (
-        <section>
-            Esto es el layout del dashboard:
-            {children}
-        </section>
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <div className="w-full flex-none md:w-64">
+            <SideNav />
+        </div>
+        <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
+    </div>
     )
 }
