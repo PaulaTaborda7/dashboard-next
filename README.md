086258f5e7b867d01d7a21c6d5286924b75e4062
PaulaTaborda7 <ptabordam@unal.edu.co>
2024-01-01 00:47:34 -0500

    Navigation check; and the componente are right in the server and in the client

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ This is the starter template for the Next.js App Router Course. It contains the 
 
 For more information, see the [course curriculum](https://nextjs.org/learn) on the Next.js Website.
 # dashboard-next
+
+
+# Please create a file .env with this variables:
+POSTGRES_URL="postgres://default:Cd9pERirfP4w@ep-winter-shadow-77985865-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+POSTGRES_PRISMA_URL="postgres://default:Cd9pERirfP4w@ep-winter-shadow-77985865-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?pgbouncer=true&connect_timeout=15"
+POSTGRES_URL_NON_POOLING="postgres://default:Cd9pERirfP4w@ep-winter-shadow-77985865.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+POSTGRES_USER="default"
+POSTGRES_HOST="ep-winter-shadow-77985865-pooler.us-east-1.postgres.vercel-storage.com"
+POSTGRES_PASSWORD="Cd9pERirfP4w"
+POSTGRES_DATABASE="verceldb"
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,8 +1,18 @@
+// With the ojective that the Pathname be useful in the client and in the server:
+'use client'
+
+
+import Link from 'next/link';
+
+// To change the styles depending of the current page
+import { usePathname } from 'next/navigation';
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -17,19 +27,28 @@ const links = [
 ];
 
 export default function NavLinks() {
+  // Read the pathname from the router
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={`flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3
+            
+            ${ pathname === link.href ? 'bg-sky-100 text-blue-600' : 'text-gray-500' }
+            `}
+          
+
+          
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
